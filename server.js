c2296bb7c5c78034f58f21f3275da2e676018e1f
Agung Setyawan <agung.01setyawan@gmail.com>
2021-11-02 21:55:34 +0000

    change free memory to available memory

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const server = require('http').createServer(serverHandler).listen(port, ip)
 const io = require('socket.io').listen(server)
 
 function formatBytes(bytes, decimals = 2) {
-  if (bytes === 0) return "0 Bytes";
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  if (bytes === 0) return '0 Bytes'
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
 }
 
 io.sockets.on('connection', function (socket) {
@@ -29,7 +29,7 @@ io.sockets.on('connection', function (socket) {
   // Function for checking memory
   execHandler("egrep --color 'MemTotal' /proc/meminfo | egrep '[0-9.]{4,}' -o")
     .then((stdout) => {
-      socket.emit("memoryTotal", formatBytes(stdout * 1024));
+      socket.emit('memoryTotal', formatBytes(stdout * 1024))
     })
     .catch(errorHandler)
 
@@ -48,10 +48,10 @@ io.sockets.on('connection', function (socket) {
   execHandler("top -d 0.5 -b -n2 | tail -n 10 | awk '{print $12}'")
     .then((stdout) => socket.emit('toplist', stdout))
     .catch(errorHandler)
-  
+
   execHandler('cat /sys/class/thermal/thermal_zone0/temp')
     .then((stdout) => {
-      const temp = parseFloat(stdout) / 1000;
+      const temp = parseFloat(stdout) / 1000
       socket.emit('temperatureUpdate', null, temp)
     })
     .catch(errorHandler)
@@ -59,7 +59,7 @@ io.sockets.on('connection', function (socket) {
   setInterval(function () {
     Promise.all([
       execHandler(
-        "egrep --color 'MemFree' /proc/meminfo | egrep '[0-9.]{4,}' -o"
+        "egrep --color 'MemAvailable' /proc/meminfo | egrep '[0-9.]{4,}' -o"
       ),
       execHandler(
         "egrep --color 'Buffers' /proc/meminfo | egrep '[0-9.]{4,}' -o"
@@ -72,16 +72,16 @@ io.sockets.on('connection', function (socket) {
       )
     ])
       .then((stdouts) => {
-        const memFree = stdouts[0]
+        const memAvailable = stdouts[0]
         const memBuffered = stdouts[1]
         const memCached = stdouts[2]
         const memTotal = stdouts[3]
 
-        const memUsed = parseInt(memTotal, 10) - parseInt(memFree, 10)
+        const memUsed = parseInt(memTotal, 10) - parseInt(memAvailable, 10)
         const percentUsed = Math.round(
           (parseInt(memUsed, 10) * 100) / parseInt(memTotal, 10)
         )
-        const percentFree = 100 - percentUsed
+        const percentAvailable = 100 - percentUsed
         const percentBuffered = Math.round(
           (parseInt(memBuffered, 10) * 100) / parseInt(memTotal, 10)
         )
@@ -91,15 +91,15 @@ io.sockets.on('connection', function (socket) {
 
         socket.emit(
           'memoryUpdate',
-          percentFree,
+          percentAvailable,
           percentUsed,
           percentBuffered,
           percentCached,
-          formatBytes(memFree * 1024),
+          formatBytes(memAvailable * 1024),
           formatBytes(memUsed * 1024),
           formatBytes(memBuffered * 1024),
           formatBytes(memCached * 1024)
-        );
+        )
       })
       .catch(errorHandler)
 
